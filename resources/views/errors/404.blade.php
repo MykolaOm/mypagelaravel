6936c7df0969f49dd35a514218bd55efec81c867
Nikolas <mykola.omelyanov@gmail.com>
2017-07-14 11:37:03 +0300

    redirection from unknown adress test1

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .center{
+            position: absolute;
+            left:30%;
+            top:30%;
+        }
+    </style>
+</head>
+<body>
+<div class="center">
+
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-3">
+        <span style="align:center;font-size:40px">PAGE NOT FOUND </span>
+        <span style="align:center;font-size:40px"> [404]? :)</span>
+        <br>
+        <span style="align:center;font-size:14px;opacity:0.4"> try other adress</span>
+    </div>
+    <div class="col-md-3"></div>
+</div>
+
+</div>
+</body>
+</html>
